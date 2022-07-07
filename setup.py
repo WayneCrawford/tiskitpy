8907726f56eb7b077e492f70757e994925b3a7bd
Wayne Crawford <crawford@ipgp.fr>
2022-07-07 09:24:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
          ]
     },
     python_requires='>=3.8',
-    classifiers=(
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: Science/Research",
@@ -35,6 +35,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Physics"
-    ),
+    ],
     keywords='oceanography, marine, OBS'
 )
