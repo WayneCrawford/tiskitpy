e5c1414a85cf7209463247c864b3df56ca43bf49
Wayne Crawford <crawford@ipgp.fr>
2022-07-07 09:37:23 +0200

    Update setup.py
    Added 'obstools' to install_requires

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=['obspy','PyYAML', 'numpy', 'scipy', 'matplotlib',
-                      'xarray'],
+                      'xarray', 'obstools'],
     entry_points={
          'console_scripts': [
          ]
