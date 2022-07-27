3db0aabd6ebb540de4974a7d38a90f1eb586afee
Wayne Crawford <crawford@ipgp.fr>
2022-07-27 17:08:00 +0200

    Update classes.py

--- docs/source/classes.py
+++ docs/source/classes.py
@@ -1,1 +1,26 @@
 
+TiSKit Classes
+=============================
+
+.. toctree::
+   :maxdepth: 4
+
+Main classes
+------------------
+
+   classes/spectral_density
+   classes/decimate
+   classes/data_cleaner
+   classes/clean_rotator
+   classes/transfer_functions
+    
+Helper classes
+------------------
+
+   classes/time_spans
+    
+Isolated classes
+------------------
+
+   classes/fir_corr
+   classes/rptransient
