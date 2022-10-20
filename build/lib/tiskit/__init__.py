63c070c5d976ad07775e822754ea81e98e9df099
Wayne Crawford <crawford@ipgp.fr>
2022-10-20 12:25:24 +0200

    Update install instructions

--- build/lib/tiskit/__init__.py
+++ build/lib/tiskit/__init__.py
@@ -8,25 +8,25 @@ them into those classes
 Classes
 =============
 
-- `CleanRotator`: rotate data to minimize noise on vertical channel
-- `DataCleaner`: Transfer_Function-based data cleaning
-- `Decimator`: Decimate time series and update metadata with the decimator's
-               response
-- `PeriodicTransient`: calculate and remove periodic transient (VERY manual!)
-- `SpectralDensity`: Calculate and manipulate spectral density functions.
-- `TimeSpans`: Specify time spans to be removed, kept, zeroed, etc.
-- `TransferFunctions`: Transfer functions for a given input channel.
+- ``CleanRotator``: rotate data to minimize noise on vertical channel
+- ``DataCleaner``: Transfer_Function-based data cleaning
+- ``Decimator``: Decimate time series and update metadata with the decimator's
+  response
+- ``PeriodicTransient``: calculate and remove periodic transient (VERY manual!)
+- ``SpectralDensity``: Calculate and manipulate spectral density functions.
+- ``TimeSpans``: Specify time spans to be removed, kept, zeroed, etc.
+- ``TransferFunctions``: Transfer functions for a given input channel.
                
 
 Functions
 =============
 
-- `fir2caus`: transform zero-phase data to minimum phase (only works for
-              LCHEAPO loggers, need to update to calculate/work for any
-              zero-phase filter)
-- `read_MSEED`: read in MSEED data, including if the file is too big (> 2 GB)
-               for obspy's read() function
-- `Peterson_noise_model``: return the Peterson High and Low Noise Models
+- ``fir2caus``: transform zero-phase data to minimum phase (only works for
+  LCHEAPO loggers, need to update to calculate/work for any
+  edzero-phase filter)
+- ``read_MSEED``: read in MSEED data, including if the file is too big (> 2 GB)
+  for obspy's read() function
+- ``Peterson_noise_model``: return the Peterson High and Low Noise Models
 
 """
 from .clean_rotator import CleanRotator
--- docs/source/install.rst
+++ docs/source/install.rst
@@ -7,10 +7,11 @@ To install Tiskit:
 1) Create a conda "obspy" environment following the instructions at
    https://github.com/obspy/obspy/wiki/Installation-via-Anaconda
 2) In your conda "obspy" environment, install tiskit by 
-    - typing ``pip install tiskit`` on the command line (latest master code)
-    - downloading the 'developer' version of tiskit from github
-      at https://github.com/WayneCrawford/tiskit, then typing
-      ``pip install .`` from within your downloaded tiskit directory
+    - for the latest "master" version, type ``pip install tiskit-py`` on the
+      command line
+    - for the latest "developer" version, clone  the 'developer' branch 
+      at https://github.com/WayneCrawford/tiskit, then type
+      ``pip install .`` from within the downloaded tiskit directory
 
-The example codes are located in the github version, under
-``tiskit/_examples/code``
+The example codes are located on github, at
+https://github.com/WayneCrawford/tiskit/tree/master/tiskit/_examples/codes
