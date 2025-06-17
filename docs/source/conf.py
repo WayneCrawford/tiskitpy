27b4c4e2b001d41665711a83bae82664318c7c99
Wayne Crawford <crawford@ipgp.fr>
2025-06-17 18:03:03 +0200

    Updated doc references to v1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, IPGP'
 author = 'Wayne Crawford' 
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.2'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
